eb846573a68195efc51d250ab3254681f7ccf478
puritrisnantya <puritrisnantya11@gmail.com>
2021-11-30 22:18:56 +0800

    add splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yum_health/interface/splash_screen.dart';
 import 'common/style.dart';
 
 void main() {
@@ -14,7 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'YumHealth',
       theme: ThemeData(
         primaryColor: primaryColor,
-        textTheme: myTextTheme,
         colorScheme:
             ColorScheme.fromSwatch().copyWith(secondary: secondaryColor),
       ),
