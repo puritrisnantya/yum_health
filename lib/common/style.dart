a703c6ea9491c27fe6f73bcc14e4a0f756ccced2
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-02 01:31:42 +0800

    add myTextTheme as TextTheme at style.dart

--- lib/common/style.dart
+++ lib/common/style.dart
@@ -1,4 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const Color primaryColor = Color(0xFF77C046);
 const secondaryColor = Color(0xFF498D43);
+
+final TextTheme myTextTheme = TextTheme(
+  headline1: GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  ),
+  headline2: GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: primaryColor,
+  ),
+  headline3: GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  ),
+  headline4: GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  ),
+  headline5: GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  ),
+  headline6: GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  ),
+  subtitle1: GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: const Color(0xFF636363),
+  ),
+  subtitle2: GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  ),
+  bodyText1: GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  ),
+  button: GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  ),
+);
