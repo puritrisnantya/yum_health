f2d1c2365677ec3393716acf314ddf9d5ce69708
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-06 22:19:34 +0800

    add the favorite_button.dart file to adjust the appearance of the favorite button

---
+++ lib/widget/favorite_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFFF1F1F1),
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      ),
+      child: IconButton(
+        icon: Icon(
+          isFavorite ? Icons.favorite : Icons.favorite_border,
+          color: Colors.red,
+        ),
+        onPressed: () {
+          setState(() {
+            isFavorite = !isFavorite;
+          });
+        },
+      ),
+    );
+  }
+}
