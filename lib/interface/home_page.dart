a867dfabb3b71f8c745b7934a85a037fc36a102e
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-06 21:14:48 +0800

    add home page

---
+++ lib/interface/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:yum_health/common/style.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: true,
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              Text(
+                'Hello, Jisoo',
+                style: myTextTheme.headline2,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
