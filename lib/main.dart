70c99c180ab6f869f4dd345a96461ce03aca7052
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-06 23:19:20 +0800

    add new route to the detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:yum_health/data/temporary_data.dart';
+import 'package:yum_health/interface/detail_page.dart';
 import 'package:yum_health/interface/landing_page.dart';
 import 'package:yum_health/interface/splash_screen.dart';
 import 'package:yum_health/utils/navigation_bar_router.dart';
@@ -25,6 +27,8 @@ class MyApp extends StatelessWidget {
         SplashScreen.routeName: (context) => const SplashScreen(),
         LandingPage.routeName: (context) => const LandingPage(),
         NavigationBarRouter.routeName: (context) => const NavigationBarRouter(),
+        DetailPage.routeName: (context) => DetailPage(
+            resep: ModalRoute.of(context)?.settings.arguments as Resep?),
       },
     );
   }
