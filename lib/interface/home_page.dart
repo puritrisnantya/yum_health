abbf0cbbb225fed89b606207085ca38fa2fa1c1a
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-06 21:18:57 +0800

    add a header on the home page

--- lib/interface/home_page.dart
+++ lib/interface/home_page.dart
@@ -24,6 +24,53 @@ class HomePage extends StatelessWidget {
                 'Want to eat healthy food?',
                 style: myTextTheme.headline4,
               ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  color: primaryColor,
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 78,
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Transform.rotate(
+                        angle: 0.5,
+                        child: Image.asset(
+                          'images/avocado.png',
+                          fit: BoxFit.fitHeight,
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: Container(
+                        padding: const EdgeInsets.all(14),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Health body comes with good nutrients',
+                              style: myTextTheme.headline6,
+                            ),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              'Geet goods nutrients now!',
+                              style: myTextTheme.subtitle2,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
