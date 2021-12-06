c30f1c66286595d73a06e87ccc3d3896a6040761
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-06 21:36:04 +0800

    make temporary data of recipes

---
+++ lib/data/temporary_data.dart
@@ -0,0 +1,53 @@
+class Resep {
+  String title;
+  String description;
+  String imageAsset;
+  String label;
+  String ingredients;
+  String steps;
+
+  Resep({
+    required this.title,
+    required this.description,
+    required this.imageAsset,
+    required this.label,
+    required this.ingredients,
+    required this.steps,
+  });
+}
+
+var resepList = [
+  Resep(
+    title: 'Sushi Hiro',
+    description:
+        'Aburi Salmon Spicy, salmonnya fresh, dagingnya empuk dan halus. Enak! Salmon Volcano Roll, ini menu favorit saya, rasanya selalu enak dan must order! Aburi Salmon Belly, ini ....',
+    imageAsset: 'images/sushi.jpg',
+    label: 'Obesitas',
+    ingredients:
+        'Nori (Rumput laut, 21x19 cm) : 1 lembar\nNasi Sushi* : ± 170 gram\nSelada : 1 lembar besar\nKepiting Kamaboko (pasta ikan) : 2 potong\nTelur Dadar Panggang : sesuai selera\nAlpukat :sesuai selera\nTelur ikan terbang : sesuai selera',
+    steps:
+        'Letakkan selembar nori di atas rolling mat, lalu sebarkan nasi di atas nori dan ratakan. Tutupi nasi dengan plastic wrap, tahan nori dengan tangan lalu balik.',
+  ),
+  Resep(
+    title: 'Sushi Hiro',
+    description:
+        'Aburi Salmon Spicy, salmonnya fresh, dagingnya empuk dan halus. Enak! Salmon Volcano Roll, ini menu favorit saya, rasanya selalu enak dan must order! Aburi Salmon Belly, ini ....',
+    imageAsset: 'images/sushi.jpg',
+    label: 'Obesitas',
+    ingredients:
+        'Nori (Rumput laut, 21x19 cm) : 1 lembar\nNasi Sushi* : ± 170 gram\nSelada : 1 lembar besar\nKepiting Kamaboko (pasta ikan) : 2 potong\nTelur Dadar Panggang : sesuai selera\nAlpukat :sesuai selera\nTelur ikan terbang : sesuai selera',
+    steps:
+        'Letakkan selembar nori di atas rolling mat, lalu sebarkan nasi di atas nori dan ratakan. Tutupi nasi dengan plastic wrap, tahan nori dengan tangan lalu balik.',
+  ),
+  Resep(
+    title: 'Sushi Hiro',
+    description:
+        'Aburi Salmon Spicy, salmonnya fresh, dagingnya empuk dan halus. Enak! Salmon Volcano Roll, ini menu favorit saya, rasanya selalu enak dan must order! Aburi Salmon Belly, ini ....',
+    imageAsset: 'images/sushi.jpg',
+    label: 'Obesitas',
+    ingredients:
+        'Nori (Rumput laut, 21x19 cm) : 1 lembar\nNasi Sushi* : ± 170 gram\nSelada : 1 lembar besar\nKepiting Kamaboko (pasta ikan) : 2 potong\nTelur Dadar Panggang : sesuai selera\nAlpukat :sesuai selera\nTelur ikan terbang : sesuai selera',
+    steps:
+        'Letakkan selembar nori di atas rolling mat, lalu sebarkan nasi di atas nori dan ratakan. Tutupi nasi dengan plastic wrap, tahan nori dengan tangan lalu balik.',
+  ),
+];
