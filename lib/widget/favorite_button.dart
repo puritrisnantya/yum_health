987749a419e7bdef63a35e95996231a6ca261d3d
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-10 10:28:02 +0800

    fix favorite button

--- lib/widget/favorite_button.dart
+++ lib/widget/favorite_button.dart
@@ -1,33 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yum_health/data/temporary_data.dart';
+import 'package:yum_health/helper/state_helper.dart';
+import 'package:yum_health/provider/change_favorite_provider.dart';
 
-class FavoriteButton extends StatefulWidget {
-  const FavoriteButton({Key? key}) : super(key: key);
+class FavoriteButton extends StatelessWidget {
+  const FavoriteButton({Key? key, required this.data}) : super(key: key);
 
-  @override
-  _FavoriteButtonState createState() => _FavoriteButtonState();
-}
-
-class _FavoriteButtonState extends State<FavoriteButton> {
-  bool isFavorite = false;
+  final Resep data;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Color(0xFFF1F1F1),
-        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-      ),
-      child: IconButton(
-        icon: Icon(
-          isFavorite ? Icons.favorite : Icons.favorite_border,
-          color: Colors.red,
+    return Consumer<ChangeFavoriteProvider>(builder: (context, newData, chill) {
+      return Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFFF1F1F1),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
-        onPressed: () {
-          setState(() {
-            isFavorite = !isFavorite;
-          });
-        },
-      ),
-    );
+        child: IconButton(
+          icon: Icon(
+            newData.icon,
+            color: Colors.red,
+          ),
+          onPressed: () async {
+            if (newData.favoriteState == ResultState.noData) {
+              await newData.addFavorite(data);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: Colors.black.withOpacity(0.6),
+                  content: const Text(
+                    'Favorite berhasil ditambahkan',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: const Duration(seconds: 1),
+                ),
+              );
+            } else {
+              await newData.deleteFavorite();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: Colors.black.withOpacity(0.6),
+                  content: const Text(
+                    'Favorite berhasil dihapus',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: const Duration(seconds: 1),
+                ),
+              );
+            }
+          },
+        ),
+      );
+    });
   }
 }
