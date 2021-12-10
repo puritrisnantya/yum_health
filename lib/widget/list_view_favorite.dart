4e13a30e5d5d3b7f2307e8b4f25c8702c535a43f
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-10 10:29:58 +0800

    add list view for favorite list

---
+++ lib/widget/list_view_favorite.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:yum_health/common/style.dart';
+import 'package:yum_health/data/temporary_data.dart';
+import 'package:yum_health/interface/detail_page.dart';
+
+class ListViewFavorite extends StatelessWidget {
+  const ListViewFavorite({Key? key, required this.resep}) : super(key: key);
+  final List<Resep> resep;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+          itemCount: resep.length,
+          itemBuilder: (BuildContext context, int index) {
+            return _listItem(context, resep[index]);
+          }),
+    );
+  }
+}
+
+Widget _listItem(BuildContext context, Resep resep) {
+  return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, DetailPage.routeName, arguments: resep);
+      },
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 10),
+        height: 118,
+        child: Card(
+          color: const Color(0xFFF1F1F1),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              Container(
+                height: 108 - 18,
+                width: 80,
+                margin: const EdgeInsets.only(bottom: 9, top: 9, left: 9),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      image: AssetImage(resep.imageAsset), fit: BoxFit.cover),
+                ),
+              ),
+              Flexible(
+                child: Container(
+                  padding: const EdgeInsets.all(14),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        resep.title,
+                        style: myTextTheme.headline5,
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        resep.description,
+                        style: myTextTheme.subtitle1,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ));
+}
