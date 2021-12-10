dae1528138108e92addc57ab9e332389bcb2bf8e
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-10 10:30:50 +0800

    add ChangeNotifierProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yum_health/data/temporary_data.dart';
 import 'package:yum_health/interface/calculate_bmi_page.dart';
 import 'package:yum_health/interface/detail_page.dart';
@@ -7,6 +8,7 @@ import 'package:yum_health/interface/profile_page.dart';
 import 'package:yum_health/interface/sign_in_page.dart';
 import 'package:yum_health/interface/sign_up_page.dart';
 import 'package:yum_health/interface/splash_screen.dart';
+import 'package:yum_health/provider/favorite_provider.dart';
 import 'package:yum_health/utils/navigation_bar_router.dart';
 import 'common/style.dart';
 
@@ -19,26 +21,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'YumHealth',
-      theme: ThemeData(
-        primaryColor: primaryColor,
-        colorScheme:
-            ColorScheme.fromSwatch().copyWith(secondary: secondaryColor),
+    return ChangeNotifierProvider<FavoriteProvider>(
+      create: (_) => FavoriteProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'YumHealth',
+        theme: ThemeData(
+          primaryColor: primaryColor,
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: secondaryColor),
+        ),
+        initialRoute: SplashScreen.routeName,
+        routes: {
+          SplashScreen.routeName: (context) => const SplashScreen(),
+          LandingPage.routeName: (context) => const LandingPage(),
+          NavigationBarRouter.routeName: (context) =>
+              const NavigationBarRouter(),
+          SignInPage.routeName: (context) => SignInPage(),
+          SignUpPage.routeName: (context) => SignUpPage(),
+          CalculatePage.routeName: (context) => CalculatePage(),
+          ProfilePage.routeName: (context) => ProfilePage(),
+          DetailPage.routeName: (context) => DetailPage(
+              resep: ModalRoute.of(context)?.settings.arguments as Resep?),
+        },
       ),
-      initialRoute: SplashScreen.routeName,
-      routes: {
-        SplashScreen.routeName: (context) => const SplashScreen(),
-        LandingPage.routeName: (context) => const LandingPage(),
-        NavigationBarRouter.routeName: (context) => const NavigationBarRouter(),
-        SignInPage.routeName: (context) => SignInPage(),
-        SignUpPage.routeName: (context) => SignUpPage(),
-        CalculatePage.routeName: (context) => CalculatePage(),
-        ProfilePage.routeName: (context) => ProfilePage(),
-        DetailPage.routeName: (context) => DetailPage(
-            resep: ModalRoute.of(context)?.settings.arguments as Resep?),
-      },
     );
   }
 }
