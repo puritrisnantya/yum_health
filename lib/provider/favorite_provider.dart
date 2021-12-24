c349347670a6c28209520e69eef3f00a0dfcb1cc
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-24 13:05:44 +0800

    update favorite_provider.dart

--- lib/provider/favorite_provider.dart
+++ lib/provider/favorite_provider.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:yum_health/data/database/favorite_database.dart';
-import 'package:yum_health/data/temporary_data.dart';
+import 'package:yum_health/data/database/auth/auth_service.dart';
+import 'package:yum_health/data/database/firestore/resep_db_service.dart';
+import 'package:yum_health/data/model/resep_model.dart';
 import 'package:yum_health/helper/state_helper.dart';
 
 class FavoriteProvider extends ChangeNotifier {
   FavoriteProvider() {
-    _dbFavorite = DatabaseFavorite();
+    _dbService = DatabaseService();
     _getAllFavorite();
+    getFavoritListData();
   }
 
-  late DatabaseFavorite _dbFavorite;
-  List<Resep> _favorite = [];
+  late DatabaseService _dbService;
+  List<String> _favorite = [];
+  late ResepData _favoriteDetail;
   late ResultState _state;
   late ResultState _favoriteState;
-  final IconData _icon = Icons.favorite_outline;
+  IconData _icon = Icons.favorite_outline;
 
-  List<Resep> get favorite => _favorite;
+  late List<ResepData> _listResep;
+  List<ResepData> get listResep => _listResep;
+
+  List<String> get favorite => _favorite;
+  ResepData get favoriteDetail => _favoriteDetail;
 
   IconData get icon => _icon;
 
@@ -27,7 +34,8 @@ class FavoriteProvider extends ChangeNotifier {
     _state = ResultState.loading;
     notifyListeners();
     try {
-      final results = await _dbFavorite.getFavorit();
+      final results =
+          await _dbService.getAllFavorite(AuthService().auth.currentUser!.uid);
       if (results.isNotEmpty) {
         _favorite = results;
         _state = ResultState.hasData;
@@ -43,7 +51,78 @@ class FavoriteProvider extends ChangeNotifier {
     }
   }
 
-  void refreshData() {
-    _getAllFavorite();
+  Future<ResepData?> getFavoriteData(String id) async {
+    try {
+      final results = await _dbService.getResepById(id);
+      return _favoriteDetail = results;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future getFavoritListData() async {
+    _favoriteState = ResultState.loading;
+    notifyListeners();
+    try {
+      _listResep = [];
+      final result =
+          await _dbService.getAllFavorite(AuthService().auth.currentUser!.uid);
+      final List<ResepData> listResep = [];
+      for (var e in result) {
+        final data = await _dbService.getResepById(e);
+        listResep.add(data);
+      }
+      _listResep = listResep;
+      if (result.isEmpty) {
+        _favoriteState = ResultState.noData;
+      } else {
+        _favoriteState = ResultState.hasData;
+      }
+      notifyListeners();
+    } catch (e) {
+      _favoriteState = ResultState.error;
+      notifyListeners();
+    }
+  }
+
+  Future getFavoriteById(String idResep) async {
+    try {
+      final result =
+          await _dbService.getAllFavorite(AuthService().auth.currentUser!.uid);
+      bool isFounded = false;
+      for (var e in result) {
+        if (e == idResep) {
+          isFounded = true;
+        }
+      }
+      if (isFounded) {
+        _favoriteState = ResultState.hasData;
+        _icon = Icons.favorite;
+        notifyListeners();
+      } else {
+        _favoriteState = ResultState.noData;
+        _icon = Icons.favorite_outline;
+        notifyListeners();
+      }
+    } catch (e) {
+      _favoriteState = ResultState.error;
+      notifyListeners();
+    }
+  }
+
+  Future<void> addFavorite(String idResep) async {
+    await _dbService.inputFavorite(
+        AuthService().auth.currentUser!.uid, idResep);
+    getFavoritListData();
+    _icon = Icons.favorite;
+    getFavoriteById(idResep);
+  }
+
+  Future<void> deleteFavorite(String idResep) async {
+    await _dbService.removeFavorite(
+        AuthService().auth.currentUser!.uid, idResep);
+    getFavoritListData();
+    _icon = Icons.favorite_outline;
+    getFavoriteById(idResep);
   }
 }
