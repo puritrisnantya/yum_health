bd7d3328a56481daf7c2d6f035e2f639c5cd8f21
puritrisnantya <puritrisnantya11@gmail.com>
2021-12-24 13:07:24 +0800

    update profile_page.dart

--- lib/interface/profile_page.dart
+++ lib/interface/profile_page.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yum_health/common/style.dart';
+import 'package:yum_health/helper/state_helper.dart';
 import 'package:yum_health/interface/sign_in_page.dart';
+import 'package:yum_health/provider/auth_provider.dart';
 
 class ProfilePage extends StatefulWidget {
   static const routeName = '/profile_page';
@@ -19,87 +22,114 @@ class _ProfilePageState extends State<ProfilePage> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-          child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Profile',
-              style: myTextTheme.headline1,
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Text(
-              'Username',
-              style: myTextTheme.headline4,
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            Container(
-              height: 50,
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(10)),
-              child: Center(
-                child: Text(
-                  'Kim_Jisoo',
-                  style: myTextTheme.headline4,
+          child: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Profile',
+                style: myTextTheme.headline1,
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Username',
+                style: myTextTheme.headline4,
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              Container(
+                height: 50,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Consumer<AuthProvider>(
+                      builder: (context, snapshot, child) {
+                    if (snapshot.state == ResultState.loading) {
+                      return const Center(
+                        child: CircularProgressIndicator(
+                          color: primaryColor,
+                        ),
+                      );
+                    } else if (snapshot.state == ResultState.hasData) {
+                      return Text(
+                        snapshot.user.username,
+                        style: myTextTheme.headline4,
+                      );
+                    } else {
+                      return const Text('Error');
+                    }
+                  }),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 25.0,
-            ),
-            Text(
-              'Email',
-              style: myTextTheme.headline4,
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            Container(
-              height: 50,
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(10)),
-              child: Center(
-                child: Text(
-                  'kimjiso95@gmail.com',
-                  style: myTextTheme.headline4,
+              const SizedBox(
+                height: 15.0,
+              ),
+              Text(
+                'Email',
+                style: myTextTheme.headline4,
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              Container(
+                height: 50,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Consumer<AuthProvider>(
+                      builder: (context, snapshot, child) {
+                    if (snapshot.state == ResultState.loading) {
+                      return const Center(
+                        child: CircularProgressIndicator(
+                          color: primaryColor,
+                        ),
+                      );
+                    } else if (snapshot.state == ResultState.hasData) {
+                      return Text(
+                        snapshot.user.email,
+                        style: myTextTheme.headline4,
+                      );
+                    } else {
+                      return const Text('Error');
+                    }
+                  }),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 60.0,
-            ),
-            TextButton(
-              onPressed: () async {
-                print('Keluar');
-                await _auth.signOut();
-                Navigator.pushReplacementNamed(context, SignInPage.routeName);
-              },
-              child: Container(
-                child: Text(
-                  'Keluar',
-                  textAlign: TextAlign.center,
-                  style: myTextTheme.button,
-                ),
-                padding: const EdgeInsets.all(8.0),
-                width: size.width - 70,
+              const SizedBox(
+                height: 30.0,
               ),
-              style: TextButton.styleFrom(
-                backgroundColor: primaryColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+              TextButton(
+                onPressed: () async {
+                  await _auth.signOut();
+                  Navigator.pushReplacementNamed(context, SignInPage.routeName);
+                },
+                child: Container(
+                  child: Text(
+                    'Keluar',
+                    textAlign: TextAlign.center,
+                    style: myTextTheme.button,
+                  ),
+                  padding: const EdgeInsets.all(8.0),
+                  width: size.width - 70,
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       )),
     );
